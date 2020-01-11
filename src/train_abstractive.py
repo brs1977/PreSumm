6ad6316d192a84fc53a01f0dbf564fe730681a73
brs1977 <47769178+brs1977@users.noreply.github.com>
2020-01-11 23:09:29 +0300

    Update train_abstractive.py

--- src/train_abstractive.py
+++ src/train_abstractive.py
@@ -153,16 +153,17 @@ def validate_abs(args, device_id):
                     step = int(cp.split('.')[-2].split('_')[-1])
                     validate(args, device_id, cp, step)
                     test_abs(args, device_id, cp, step)
-
-            cp_files = sorted(glob.glob(os.path.join(args.model_path, 'model_step_*.pt')))
-            cp_files.sort(key=os.path.getmtime)
-            if (cp_files):
-                cp = cp_files[-1]
-                time_of_cp = os.path.getmtime(cp)
-                if (time_of_cp > timestep):
-                    continue
-            else:
-                time.sleep(300)
+                    break
+                    
+#             cp_files = sorted(glob.glob(os.path.join(args.model_path, 'model_step_*.pt')))
+#             cp_files.sort(key=os.path.getmtime)
+#             if (cp_files):
+#                 cp = cp_files[-1]
+#                 time_of_cp = os.path.getmtime(cp)
+#                 if (time_of_cp > timestep):
+#                     continue
+#             else:
+#                 time.sleep(300)
 
 
 def validate(args, device_id, pt, step):

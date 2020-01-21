e7ad7657bd7295cc7db3390ccfb14d40cb08c4f4
r.bukin <r.bukin@citsp.ru>
2020-01-21 16:04:49 +0300

    predict

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ from __future__ import division
 import argparse
 import os
 from others.logging import init_logger
-from train_abstractive import validate_abs, train_abs, baseline, test_abs, test_text_abs
+from train_abstractive import validate_abs, train_abs, baseline, test_abs, test_text_abs, predict_abs
 from train_extractive import train_ext, validate_ext, test_ext
 
 model_flags = ['hidden_size', 'ff_size', 'heads', 'emb_size', 'enc_layers', 'enc_hidden_size', 'enc_ff_size',
